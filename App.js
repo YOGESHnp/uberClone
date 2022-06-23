e36c5e2dfc0311aad4cb15ec4f84514813825a86
YOGESHnp <yogeshdev83@gmail.com>
2022-06-23 22:56:36 +0530

    Updating App.js after creating RestaurantItem component

--- App.js
+++ App.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import {View, Text, StyleSheet, Image, StatusBar} from 'react-native';
+import RestaurantItem from './src/components/RestaurantItem';
 
 export default function App() {
     return(
         <View style={styles.container}>
-            {/* Single Restaurant Item */}
-            <View style={styles.restaurantContainer}>
-                <Image source={{uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/uber-eats/restaurant1.jpeg'}}
-                style={styles.image}
-                />
-                <Text style={styles.title}>El Cabo Coffee Bar Tres De May</Text>
-                <Text style={styles.subtitle}>$1.99 15-30 minutes</Text>
+            
+            <RestaurantItem/>
+            <RestaurantItem/>
 
-            </View>
-        
             <StatusBar style="auto" />
         </View>
     );
@@ -27,22 +22,6 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         padding: 10,
     },
-    restaurantContainer:{
-        width: "100%",
-    },
-    image: {
-        width: "100%",
-        aspectRatio: 5/3,
-        marginBottom: 5,
-    },
-    title: {
-        fontSize: 16,
-        fontWeight: "700",
-        marginVertical: 5,
-    },
-    subtitle: {
-        color: "grey",
-        
-    }
+    
 });
 
